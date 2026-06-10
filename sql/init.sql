ededaf06fcdf318f0565d393211a4c80b325b88a
Wang Chenzi <96861798+yyriri@users.noreply.github.com>
2026-06-10 17:42:27 +0800

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,9 @@
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    stock INTEGER
+);
+
 UPDATE products SET name = '限定谷子A' WHERE id = 1;
 UPDATE products SET name = '限定谷子B' WHERE id = 2;
 UPDATE products SET name = '限定谷子C' WHERE id = 3;
